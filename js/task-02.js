897c39937a2b82682e8e5bb2f18161fc653d764d
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-10-30 19:03:10 +0300

    Выполняет вторую задачу

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +8,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const listItemEl = document.createElement('li');
+
+  listItemEl.classList.add('item');
+  listItemEl.textContent = ingredients[i];
+  ingredientsListEl.append(listItemEl);
+}
