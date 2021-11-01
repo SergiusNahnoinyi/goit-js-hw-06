546dd3a0c4754af59b7f141e1a28670da390e533
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-01 10:44:53 +0200

    Выполняет третью задачу

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +14,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryListEl = document.querySelector('.gallery');
+
+const galleryListMarkup = images
+  .map(
+    image =>
+      `<li class="gallery__item"><img src = ${image.url}  alt = '${image.alt}'></li>`,
+  )
+  .join('');
+
+galleryListEl.insertAdjacentHTML('beforeend', galleryListMarkup);
