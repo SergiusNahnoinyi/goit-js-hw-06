2a5dc6019b31eac61c51795faeed757ddf071ea9
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-03 10:31:24 +0200

    Выполняет пятую задачу

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const textInput = document.querySelector('#name-input');
+const textOutput = document.querySelector('#name-output');
+
+textInput.addEventListener('input', event => {
+  if (textInput.value.length === 0) {
+    textOutput.textContent = 'Anonymous';
+  } else {
+    textOutput.textContent = event.currentTarget.value;
+  }
+});
