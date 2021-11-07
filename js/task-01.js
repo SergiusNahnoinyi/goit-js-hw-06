b3edcfa165df2e48773777b09646687dc94782cb
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-07 15:27:31 +0200

    Обновляет решение первой задачи

--- js/task-01.js
+++ js/task-01.js
@@ -1,39 +1,10 @@
 'use strict';
 
-// Кол-во категорий
 const categoriesListEl = document.querySelector('#categories');
+console.log('Number of categories:', categoriesListEl.childElementCount);
 
-const categoriesCount = categoriesListEl.childElementCount;
-console.log('Number of categories:', categoriesCount);
-
-// Категория: Animals
-const firstCategoriesItem = categoriesListEl.firstElementChild;
-
-const firstCategoriesItemTitle = firstCategoriesItem.firstElementChild;
-console.log('Category:', firstCategoriesItemTitle.textContent);
-
-const firstCategoriesItemList = firstCategoriesItem.lastElementChild;
-const firstCategoriesElementsCount = firstCategoriesItemList.childElementCount;
-console.log('Elements:', firstCategoriesElementsCount);
-
-// Категория: Products
-const secondCategoriesItem = firstCategoriesItem.nextElementSibling;
-
-const secondCategoriesItemTitle = secondCategoriesItem.firstElementChild;
-console.log('Category:', secondCategoriesItemTitle.textContent);
-
-const secondCategoriesItemList = secondCategoriesItem.lastElementChild;
-
-const secondCategoriesElementsCount =
-  secondCategoriesItemList.childElementCount;
-console.log('Elements:', secondCategoriesElementsCount);
-
-// Категория: Technologies
-const lastCategoriesItem = categoriesListEl.lastElementChild;
-
-const lastCategoriesItemTitle = lastCategoriesItem.firstElementChild;
-console.log('Category:', lastCategoriesItemTitle.textContent);
-
-const lastCategoriesItemList = lastCategoriesItem.lastElementChild;
-const lastCategoriesElementsCount = lastCategoriesItemList.childElementCount;
-console.log('Elements:', lastCategoriesElementsCount);
+const categoriesItemEl = document.querySelectorAll('li.item');
+categoriesItemEl.forEach(item => {
+  console.log('Category:', item.firstElementChild.textContent);
+  console.log('Elements:', item.lastElementChild.childElementCount);
+});
