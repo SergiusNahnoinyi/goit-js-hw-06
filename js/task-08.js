48432716bd2302a5d9ebf399d7eba63ada688108
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-07 14:21:56 +0200

    Выполняет восьмую задачу

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+'use strict';
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const formData = new FormData(event.currentTarget);
+
+  if (formElements.email.value === '' || formElements.password.value === '') {
+    return alert('Please fill in all the fields!');
+  } else
+    formData.forEach((value, name) => {
+      console.log(name, value);
+    });
+
+  event.currentTarget.reset();
+}
