cf5f33743ee78ed7f49f6c0685432d2a3e1e35ce
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-08 08:46:43 +0200

    Исправляет решение второй задачи
    Поэлементно пушим в пустой массив, а уже потом спредим его в список ингридиентов

--- js/task-02.js
+++ js/task-02.js
@@ -10,11 +10,13 @@ const ingredients = [
 ];
 
 const ingredientsListEl = document.querySelector('#ingredients');
+const ingredientsArray = [];
 
 for (let i = 0; i < ingredients.length; i += 1) {
   const listItemEl = document.createElement('li');
-
   listItemEl.classList.add('item');
   listItemEl.textContent = ingredients[i];
-  ingredientsListEl.append(listItemEl);
+  ingredientsArray.push(listItemEl);
 }
+
+ingredientsListEl.append(...ingredientsArray);
